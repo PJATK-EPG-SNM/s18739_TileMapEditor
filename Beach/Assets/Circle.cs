ad85b0dff8399a366e13c98ea3f798f1cbe2f24c
s18739 <61964983+s18739@users.noreply.github.com>
2020-03-18 14:02:51 +0100

    Praca_Domowa_01
    Przepraszam, że do złego repo wrzucam ale dopiero po zrobieniu projektu zobaczyłam, że nie zrobiłam repo przed jego rozpoczęciem. Nie wiem też czy pliki można przerzucać, nigdzie nie widziałam takiej opcji.

---
+++ Beach/Assets/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+
+    public float speed = 500;
+    public float jump = 200;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+
+        if (Input.GetKeyDown("space") && (isGrounded == true)) {
+            rb.AddForce(new Vector2(0, jump));
+            isGrounded = false;
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        isGrounded = true;
+
+    }
+}
